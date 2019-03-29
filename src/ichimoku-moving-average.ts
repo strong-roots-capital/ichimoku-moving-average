a45f62aa835d37f43dd8296a74c49ca9d3c783dd
Amchelle Clendenin <amchelle.clendenin@utexas.edu>
2019-03-29 10:12:22 -0500

    Accept highs and lows

--- src/ichimoku-moving-average.ts
+++ src/ichimoku-moving-average.ts
@@ -11,7 +11,10 @@ import RingBuffer from 'ringbufferjs'
  * period, defined as the average of the highest value and the lowest
  * value.
  */
-export default class IchimokuMovingAverage extends RingBuffer<number> {
+export default class IchimokuMovingAverage {
+
+    highs: RingBuffer<number>
+    lows: RingBuffer<number>
 
     /**
      * Construct an Ichimoku moving average with specified lookback.
@@ -19,7 +22,8 @@ export default class IchimokuMovingAverage extends RingBuffer<number> {
      * @param lookback - Lookback length of this Ichimoku moving average
      */
     constructor(lookback: number) {
-        super(lookback)
+        this.highs = new RingBuffer(lookback)
+        this.lows = new RingBuffer(lookback)
     }
 
     /**
@@ -29,19 +33,21 @@ export default class IchimokuMovingAverage extends RingBuffer<number> {
      * @return Ichimoku moving average over the current lookback period
      */
     value(): number {
-        const elements = this._elements.filter(e => e !== null)
-        const high = Math.max(...elements)
-        const low = Math.min(...elements)
+        const high = Math.max(...this.highs._elements.filter(e => e !== null))
+        const low = Math.min(...this.lows._elements.filter(e => e !== null))
         return (high + low)/2
     }
 
     /**
      * Push a value into the current lookback period.
      *
-     * @param close - Value (e.g. candle close) to push into the
-     * current lookback period
+     * @param high - Value (e.g. candle close) to push into the
+     * current lookback period of highs
+     * @param low - Value to push into the current lookback period
+     * of lows
      */
-    push(close: number) {
-        this.enq(close)
+    push(high: number, low: number) {
+        this.highs.enq(high)
+        this.lows.enq(low)
     }
 }
--- test/test-ichimoku-moving-average.ts
+++ test/test-ichimoku-moving-average.ts
@@ -11,20 +11,22 @@ function clone<T = number>(list: T[]): T[] {
     return JSON.parse(JSON.stringify(list))
 }
 
-const averageValueShouldBeAverageOfHighAndLow = (t: any, list: number[], expected: number) => {
-    t.is(expected, (Math.max(...list) + Math.min(...list))/2)
+const averageValueShouldBeAverageOfHighAndLow = (t: any, highs: number[], lows: number[], expected: number) => {
+    const highestHigh = Math.max(...highs)
+    const lowestLow = Math.min(...lows)
+    t.is(expected, (highestHigh + lowestLow)/2)
 }
 
-averageValueShouldBeAverageOfHighAndLow.title = (_ = '', list: number[], expected: number) => `should calculate ichimoku-average of ${JSON.stringify(list)} to be ${expected}`
+averageValueShouldBeAverageOfHighAndLow.title = (_ = '', highs: number[], lows: number[], expected: number) => `should calculate ichimoku-average of ${JSON.stringify(highs)} and ${JSON.stringify(lows)} to be ${expected}`
 
 const tenkan = new IchimokuMovingAverage(9)
 const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for (const value of values) {
-    tenkan.push(value)
+    tenkan.push(value + 10, value)
     test(
         averageValueShouldBeAverageOfHighAndLow,
-        clone((tenkan as RingBuffer<number>)
-              ._elements.filter(e => e !== null)),
+        clone(tenkan.highs._elements.filter(e => e !== null)),
+        clone(tenkan.lows._elements.filter(e => e !== null)),
         tenkan.value()
     )
 }
